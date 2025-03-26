992a4daef3541d3c9b984a42e0b71d557f6efd02
Felix <felix.reidl@gmail.com>
2025-03-26 16:22:48 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,243 @@
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+
+mod sreach;
+
+use itertools::Itertools;
+use sreach::SReachTraceOracle;
+use clap::{Parser, ValueEnum};
+use fxhash::{FxHashMap, FxHashSet};
+use graphbench::algorithms::LinearGraphAlgorithms;
+use graphbench::degengraph::DegenGraph;
+use graphbench::graph::*;
+use graphbench::editgraph::EditGraph;
+use graphbench::io::*;
+
+use rand::{Rng, SeedableRng};
+use rand::rngs::StdRng;
+
+#[derive(Parser, Debug)]
+#[clap(author, version="1.0", about, long_about = None)]
+struct Args {
+    #[clap(value_enum)]
+    query: QueryType,    
+
+    #[clap(value_enum)]
+    method: QueryMethod,
+
+    #[clap(value_enum)]
+    size: QuerySize,    
+
+    number:usize,
+
+    /// The network file
+    file: String,
+
+    // Random seed
+    #[clap(short,long)]
+    seed:Option<u64>,
+}
+
+#[derive(Clone, Debug, ValueEnum)]
+enum QueryType {
+    Trace,
+    #[clap(name="trace_count")]
+    TraceCount,
+    Neighbours
+}
+
+#[derive(Clone, Debug, ValueEnum)]
+enum QueryMethod {
+    Basic,
+    #[clap(name="sreach2")]
+    SReach2
+}
+
+#[derive(Clone, Debug, ValueEnum)]
+enum QuerySize {
+    #[clap(name="const_10")]
+    Const10,
+    #[clap(name="const_50")]
+    Const50,
+    Log,
+    Sqrt
+}
+
+const COUNT_MOD:usize = 100000;
+
+fn main() -> Result<(), &'static str> {
+    let args = Args::parse();
+    let filename = args.file;
+
+    // Load graph
+    let mut graph = match EditGraph::from_file(&filename) {
+        Ok(G) => G,
+        Err(msg) => {
+            println!("{msg}");
+            return Err("Parsing error");
+        }
+    };
+
+    graph.remove_loops();
+    let graph = DegenGraph::from_graph(&graph);
+
+    let degen = graph.left_degrees().into_values().max().expect("Graph is empty");
+    let sreach2 = graph.sreach_sizes(2).into_values().max().unwrap();
+    
+    let n = graph.num_vertices();
+    println!(
+        "Loaded graph with n={}, m={}, d={}, s2={}",
+        graph.num_vertices(),
+        graph.num_edges(),
+        degen,
+        sreach2
+    );
+
+    let query_size:usize = match args.size {
+        QuerySize::Const10 => 10,
+        QuerySize::Const50 => 50,
+        QuerySize::Log => (n.ilog2()+1) as usize,
+        QuerySize::Sqrt => (n.isqrt()+1) as usize,
+    };
+    let num_queries = args.number;
+
+    let large_deg_set = (query_size*10) as usize;
+    let elements: Vec<u32> = graph.degrees().into_iter()
+            .sorted_by_key(|(_,deg)| u32::MAX - deg)
+            .map(|(u,_)| u).take(large_deg_set).collect_vec();
+    println!("Running {num_queries} queries of size {query_size} ({:?}) in large-degree set of size {}", args.size, elements.len());
+
+    let rng = if let Some(seed) = args.seed {
+        StdRng::seed_from_u64(seed)
+    } else {
+        StdRng::from_os_rng()
+    };
+
+    let query_iter = RandomSetIter::new(elements, rng, query_size).take(num_queries);
+    
+    let total = match (args.method, args.query) {
+        (QueryMethod::Basic, QueryType::Trace) => trace_basic(&graph, query_iter),
+        (QueryMethod::Basic, QueryType::TraceCount) => trace_count_basic(&graph, query_iter),
+        (QueryMethod::Basic, QueryType::Neighbours) => neighbours_basic(&graph, query_iter),
+        (QueryMethod::SReach2, QueryType::Trace) => trace_sreach(&graph, query_iter),
+        (QueryMethod::SReach2, QueryType::TraceCount) => trace_count_sreach(&graph, query_iter),
+        (QueryMethod::SReach2, QueryType::Neighbours) => neighbours_sreach(&graph, query_iter),
+    };
+
+    println!("Debug counter value is {total}");
+
+    Ok(())
+}
+
+pub fn trace_count_basic(graph:&DegenGraph, queries:impl Iterator<Item=Vec<u32>>) -> usize {
+    let mut res = 0;
+    for query in queries {
+        let query_neighs:VertexSet = graph.neighbourhood(query.iter()).iter().cloned().collect();
+        let query:VertexSet = query.into_iter().collect();
+        let mut traces:FxHashMap<Vec<u32>,usize> = FxHashMap::default();
+        for u in query_neighs {
+            let u_neighs:VertexSet = graph.neighbours(&u).cloned().collect();
+            let mut trace:Vec<Vertex> = query.intersection(&u_neighs).cloned().collect();
+            trace.sort_unstable();
+            *traces.entry(trace).or_default() += 1;
+        }
+        res = (res + traces.into_iter().map(|(trace,count)| (trace.len() * count) % COUNT_MOD ).sum::<usize>()) % COUNT_MOD;
+    }
+    res
+}
+
+pub fn trace_basic(graph:&DegenGraph, queries:impl Iterator<Item=Vec<u32>>) -> usize {
+    let mut res = 0;
+    for query in queries {
+        let query_neighs:VertexSet = graph.neighbourhood(query.iter()).iter().cloned().collect();
+        let query:VertexSet = query.into_iter().collect();
+        let mut traces:FxHashSet<Vec<u32>> = FxHashSet::default();
+        for u in query_neighs {
+            let u_neighs:VertexSet = graph.neighbours(&u).cloned().collect();
+            let mut trace:Vec<Vertex> = query.intersection(&u_neighs).cloned().collect();
+            trace.sort_unstable();
+            traces.insert(trace);
+        }
+        res = (res + traces.into_iter().map(|trace| trace.len() % COUNT_MOD ).sum::<usize>()) % COUNT_MOD;
+    }
+    res
+}
+
+pub fn neighbours_basic(graph:&DegenGraph, queries:impl Iterator<Item=Vec<u32>>) -> usize {
+    let mut res = 0;
+    for query in queries {
+        res = (res + graph.neighbourhood(query.iter()).len()) % COUNT_MOD;
+    }
+    res
+}
+
+pub fn trace_sreach(graph:&DegenGraph, queries:impl Iterator<Item=Vec<u32>>) -> usize  {
+    let mut res = 0;
+    let oracle = SReachTraceOracle::for_graph(graph);
+    println!("Oracle constructed");
+    for mut query in queries {
+        query.sort_unstable_by_key(|x| graph.index_of(x));
+        let traces = oracle.compute_traces(&query, graph);
+        res = (res + traces.into_iter().filter(|(_,count)| count > &0).map(|(trace,_)| trace.len() % COUNT_MOD ).sum::<usize>()) % COUNT_MOD;
+    }
+    res
+}
+
+pub fn trace_count_sreach(graph:&DegenGraph, queries:impl Iterator<Item=Vec<u32>>) -> usize  {
+    let mut res = 0;
+    let oracle = SReachTraceOracle::for_graph(graph);
+    println!("Oracle constructed");
+    for mut query in queries {
+        query.sort_unstable_by_key(|x| graph.index_of(x));
+        let traces = oracle.compute_traces(&query, graph);
+        res = (res + traces.into_iter().map(|(trace,count)| (trace.len() * count) % COUNT_MOD ).sum::<usize>()) % COUNT_MOD;
+    }
+    res
+}
+
+pub fn neighbours_sreach(graph:&DegenGraph, queries:impl Iterator<Item=Vec<u32>>) -> usize  {
+    let mut res = 0;
+    let oracle = SReachTraceOracle::for_graph(graph);
+    for mut query in queries {
+        query.sort_unstable_by_key(|x| graph.index_of(x));
+        res = (res + oracle.compute_neighbour_count(&query, graph)) % COUNT_MOD;
+    }
+    res
+}
+
+
+struct RandomSetIter {
+    elements:Vec<u32>,
+    current:Vec<u32>,
+    current_indices:FxHashSet<usize>,
+    size:usize,
+    rng:StdRng
+}
+
+impl Iterator for RandomSetIter {
+    type Item = Vec<u32>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.current_indices.clear();
+        self.current.clear();
+
+        while self.current.len() < self.size {
+            let i = self.rng.random_range(0..self.elements.len());
+            if !self.current_indices.contains(&i) {
+                self.current_indices.insert(i);
+                self.current.push(self.elements[i]);
+            }
+        }
+
+       Some(self.current.clone())
+    }
+}
+
+impl RandomSetIter {
+    pub fn new(elements:Vec<u32>, rng:StdRng, size:usize) -> Self {
+        assert!(elements.len() >= size);
+        let current = Vec::with_capacity(size);
+        let current_indices = FxHashSet::default();
+        RandomSetIter{ elements, current, current_indices, size, rng }
+    }
+}
---
+++ src/sreach.rs
@@ -0,0 +1,428 @@
+
+use std::borrow::Borrow;
+
+use graphbench::{algorithms::LinearGraphAlgorithms, degengraph::DegenGraph, graph::*};
+
+use bitvec::prelude::*;
+use fxhash::{FxHashMap, FxHashSet};
+use itertools::Itertools;
+
+type VertexBitVec = BitVec<u64,Lsb0>;
+
+#[derive(Debug)]
+struct SReachContext {
+    context:Vec<Vertex>,
+    indices:VertexMap<usize>,
+    right_neighbours:FxHashMap<VertexBitVec, usize>
+}
+
+impl SReachContext {
+    fn new(context:Vec<Vertex>) -> Self {
+        let mut indices = VertexMap::default();
+        for (i, x) in context.iter().enumerate() {
+            indices.insert(*x, i);
+        }
+
+        let right_neighbours = Default::default();
+        SReachContext{context, indices, right_neighbours}
+    }
+
+    fn get_sets(&self) ->  FxHashMap<Vec<Vertex>, usize> {
+        self.count_neighbours(&self.context)
+    }
+
+    fn insert(&mut self, neighs:&[Vertex]) {
+        let key = self.to_bitset(neighs);
+        *self.right_neighbours.entry(key).or_default() += 1;
+    }
+
+    fn count_neighbours(&self, target:&[Vertex]) -> FxHashMap<Vec<Vertex>, usize> {
+        let mask = self.to_bitset(target);    
+
+        // We first count using the bit vectors and later convert the found bit vectors
+        // to vecs with vertices.     
+        let mut counts:FxHashMap<VertexBitVec, usize> = FxHashMap::default();
+
+        for (bits, count) in self.right_neighbours.iter() {
+            let bits = mask.clone() & bits;
+            *counts.entry(bits).or_default() += count;
+        }
+        counts.into_iter().map(|(bits,count)| (self.to_vecset(&bits), count)).collect()
+    }
+
+    /// Aggregates counts for all sets in this storage that intersect the `target` set in
+    /// exactly the rightmost element in the context (e.g. the 'anchor' vertex for this struct)
+    fn count_first_intersection(&self, target:&[Vertex]) -> usize {
+        let mask = self.to_bitset(target);    
+        let anchor_mask = self.singleton_bitset(&self.context[self.context.len()-1]);
+
+        let mut res:usize = 0;
+
+        for (bits, count) in self.right_neighbours.iter() {
+            let bits = mask.clone() & bits;
+            if bits == anchor_mask {
+                res += count;
+            }
+        }
+        res
+    }    
+
+    fn singleton_bitset(&self, u:&Vertex) -> VertexBitVec {
+        let mut bitvec:VertexBitVec = BitVec::with_capacity(self.context.len());
+        bitvec.resize(self.context.len(), false);
+        if let Some(&ix) = self.indices.get(u) {
+            bitvec.set(ix as usize, true);
+        }
+        bitvec
+    }    
+
+    fn to_bitset(&self, set:&[Vertex]) -> VertexBitVec {
+        let mut bitvec:VertexBitVec = BitVec::with_capacity(self.context.len());
+        bitvec.resize(self.context.len(), false);
+        for u in set {
+            if let Some(&ix) = self.indices.get(u) {
+                bitvec.set(ix, true);
+            }
+        }
+        bitvec
+    }
+
+    fn to_vecset(&self, bits:&VertexBitVec) -> Vec<Vertex> {
+        let mut res = vec![];
+        for ix in bits.iter_ones() {
+            res.push(*&self.context[ix]);
+        }
+        res
+    }
+}
+
+pub struct SReachTraceOracle {
+    contexts:FxHashMap<Vertex, SReachContext>
+}
+
+impl SReachTraceOracle {
+    pub fn for_graph(graph:&DegenGraph) -> Self {
+        let mut contexts:FxHashMap<_,_> = Default::default();
+
+        for x in graph.vertices() {
+            let mut S:Vec<Vertex> = graph.sreach_set(x, 2).into_iter().map(|(x,_)| x).collect();
+            S.push(*x);
+            S.sort_by_key(|x| graph.index_of(x));
+            S.dedup();
+
+            contexts.insert(*x, SReachContext::new(S));
+        }
+
+        for x in graph.vertices() {
+            let mut N = graph.left_neighbours_slice(x).iter().cloned().collect_vec();
+            N.sort_by_key(|x| graph.index_of(x));
+            
+            for i in 1..=N.len() { // All non-empty prefixes of N, including N itself
+                let Nprefix = &N[..i];
+                let anchor = *&N[i-1];
+                let ctx = contexts.get_mut(&anchor).unwrap();
+                ctx.insert(Nprefix);
+            }
+        }
+
+        Self{contexts}
+    }
+
+    pub fn compute_traces(&self, inner: &Vec<Vertex>, graph:&DegenGraph) -> FxHashMap<Vec<Vertex>, usize>  {
+        // DEBUG: print all sreach info
+        // for v in graph.vertices() {
+        //     let ctx = &self.contexts[v];
+        //     let sreach = &ctx.context;
+        //     let subsets = ctx.get_sets().into_iter().collect_vec();
+        //     println!("{v} {sreach:?}: {subsets:?}");
+        // }
+
+        // Count neighbourhoods
+        let mut traces:FxHashMap<Vec<Vertex>, usize> = FxHashMap::default();
+
+        debug_assert!(inner.is_sorted_by_key(|x| graph.index_of(x)));
+
+        traces.insert(vec![], graph.num_vertices());
+
+        // println!("Computing traces via sreach data");
+
+        // Count neighbourhoods induced by vertices to the right using the 
+        // sreach context sets
+        for i in 1..=inner.len() { // All non-empty prefixes of `inner`, including `inner` itself
+            let prefix = &inner[..i];
+            let anchor = *&inner[i-1];
+            let ctx = &self.contexts[&anchor];
+            
+            let counts = ctx.count_neighbours(&prefix);
+            for (set, count) in counts.into_iter() {
+                if set.is_empty() {
+                     continue // TODO: Is there a nicer way to filter this?
+                }
+                debug_assert!(set.is_sorted_by_key(|x| graph.index_of(x)));
+
+                // println!("  {set:?} ({count})");
+                // We avoid using the entry API here because it forces us to
+                // always clone `set`
+                match traces.get_mut(&set)  {
+                    Some(trace_count) => *trace_count += count,
+                    None => { traces.insert(set.clone(), count); },
+                };
+                
+
+                // Correct count for prefix
+                let set_prefix = &set[..set.len()-1];
+                unsafe {
+                    *traces.get_mut(set_prefix).unwrap_unchecked() -= count; // This entry must exist
+                }
+            }
+        }
+
+        // println!("Correcting left-neighbours counts");
+        // println!("Right traces {traces:?}");        
+
+        // Add neighbourhoods induced by vertices to the left 
+        let mut left_neighbours: VertexSet = VertexSet::default();
+        for u in inner.iter() {
+            left_neighbours.extend(graph.left_neighbours_slice(u).into_iter());
+            left_neighbours.insert(*u);
+        }
+
+        // println!(" -- Collected {} left neighbours", left_neighbours.len());
+
+        for y in left_neighbours.into_iter() {
+            let mut trace = vec![];
+            
+            // `trace` will inherit the ordering of `inner`
+            let mut trace_prefix = vec![];
+            for u in inner.iter() {
+                if graph.adjacent(u, &y) {
+                    trace.push(*u); 
+                    if graph.adjacent_ordered(u, &y) {
+                        trace_prefix.push(*u);
+                    }
+                }
+            }
+
+            // Only count trace if the source vertices lies outside of `inner`
+            if !inner.contains(&y) {
+                match traces.get_mut(&trace) {
+                    Some(trace_counter) => *trace_counter += 1,
+                    None => {traces.insert(trace.clone(), 1);},
+                }
+            }
+
+            // Correct count for prefix. This key must exist in `traces`
+            unsafe {
+                *traces.get_mut(&trace_prefix).unwrap_unchecked() -= 1;    
+            }
+        }
+        // println!("Final traces {traces:?}");
+
+        traces
+    }
+
+    pub fn compute_neighbour_count(&self, inner: &[Vertex], graph:&DegenGraph) -> usize {
+        let mut res:usize = 0;
+
+        debug_assert!(inner.is_sorted_by_key(|x| graph.index_of(x)));
+
+        // Count neighbourhoods induced by vertices to the right using the 
+        // sreach context sets
+        for i in 1..=inner.len() { // All non-empty prefixes of `inner`, including `inner` itself
+            let prefix = &inner[..i];
+            let anchor = *&inner[i-1];
+            let ctx = &self.contexts[&anchor];
+            
+            res += ctx.count_first_intersection(&prefix);
+        }
+
+
+        // Add neighbourhoods induced by vertices to the left 
+        let mut left_neighbours: VertexSet = VertexSet::default();
+        for u in inner.iter() {
+            left_neighbours.extend(graph.left_neighbours_slice(u).into_iter());
+            left_neighbours.insert(*u);
+        }
+
+        'outer: for u in left_neighbours.into_iter() {
+            let is_inner = inner.contains(&u);
+
+            for w in graph.left_neighbours_slice(&u) {
+                if inner.contains(w) {
+                    // The vertex u has been counted already in the first phase. If this 
+                    // vertex is in `inner`, we need to correct that.
+                    if is_inner {
+                        res -= 1;
+                    }
+                    continue 'outer;
+                }
+            }
+
+            if is_inner { 
+                continue;
+            }
+
+            res += 1;
+        }
+
+        res
+    }    
+
+    pub fn is_shattered<V,I>(&self, inner: I, k:u32, graph:&DegenGraph) -> bool where V: Borrow<u32>, I:IntoIterator<Item=V> {
+        let mut inner = inner.into_iter().map(|u| *u.borrow()).collect_vec();
+        inner.sort_by_key(|x| graph.index_of(x));
+
+        let traces = self.compute_traces(&inner, graph);
+
+        assert!(k >= inner.len() as u32);
+        let multiplicity = 2usize.pow(k - inner.len() as u32);
+        let num_neighbourhoods = 2usize.pow(inner.len() as u32);
+
+        if traces.len() < num_neighbourhoods {
+            return false
+        }
+
+        for (_N, count) in traces.into_iter() {
+            if count < multiplicity {
+                return false;
+            }
+        }
+
+        true
+    }
+}
+
+
+#[cfg(test)]
+mod  tests {
+    use super::SReachTraceOracle;
+
+    use super::*;    
+    use graphbench::{editgraph::EditGraph, graph::MutableGraph, io::*};
+    use rand::prelude::*;
+    use std::collections::BTreeSet;
+
+
+    #[test]
+    fn shattered_test_DNC () {
+        let mut graph = EditGraph::from_gzipped("networks/DNC-emails.txt.gz").expect("File not found.");
+        graph.remove_loops();
+
+        let degen = DegenGraph::from_graph(&graph);
+        let sreach_oracle = SReachTraceOracle::for_graph(&degen);
+
+        // [772, 1734, 1042]
+        println!("[772, 1734, 1042] ? {}", sreach_oracle.is_shattered([772, 1734, 1042], 3, &degen));
+        println!();
+        assert!(sreach_oracle.is_shattered([772, 1734, 1042], 3, &degen));
+
+        // [472, 1042, 1313, 1811]
+        assert!(sreach_oracle.is_shattered([472, 1042, 1313, 1811], 4, &degen));
+        
+        // [379, 772, 1248, 1614, 1635]
+        assert!(sreach_oracle.is_shattered([379, 772, 1248, 1614, 1635], 5, &degen));
+    }
+
+    #[test]
+    fn sreach_traces () {
+        let mut graph = EditGraph::new();
+
+        graph.add_vertices(vec![1,2,3,4,5,6,7,8,9,10,11].iter());
+        graph.add_edges(vec![(1,4),(2,4),(3,4),(1,5),(2,5),(3,5),(1,6),(2,6),(2,7),(3,7),(1,8),(3,8),(1,9),(2,10),(3,11)].into_iter());
+
+        // graph.add_edge(&1,&2);
+        // graph.add_edge(&2,&3);
+
+        let mut order = vec![1,2,3,4,5,6,7,8,9,10,11];
+
+        use rand::thread_rng;
+        order.shuffle(&mut thread_rng());
+
+        let degen = DegenGraph::with_ordering(&graph, order);
+        let sreach_oracle = SReachTraceOracle::for_graph(&degen);
+
+        let mut target = vec![1,2,3];
+        target.sort_by_key(|x| degen.index_of(x));
+        let traces = helper_compute_traces(&target, &degen);
+        let traces_sreach = sreach_oracle.compute_traces(&target, &degen);
+
+        // The sreach_traces might contain 0-count sets which make the test fail
+        let traces_sreach:FxHashMap<_,_> = traces_sreach.into_iter().filter(|(k,v)| *v > 0 ).collect();
+
+        println!("traces: {traces:?}");
+        println!("traces: {traces_sreach:?}");
+
+        assert_eq!(traces_sreach, traces);
+    }
+
+
+    #[test]
+    fn sreach_traces_2 () {
+        let mut graph = EditGraph::new();
+
+        // 1 -- 2 -- 3
+        graph.add_vertices(vec![1,2,3].iter());
+        graph.add_edge(&1, &2);
+        graph.add_edge(&2, &3);
+
+        let degen = DegenGraph::with_ordering(&graph, vec![1,2,3]);
+        let sreach_oracle = SReachTraceOracle::for_graph(&degen);
+
+        let mut target = vec![1,2,3];
+        let traces = helper_compute_traces(&target, &degen);
+
+        let traces_sreach = sreach_oracle.compute_traces(&target, &degen);
+
+        // The sreach_traces might contain 0-count sets which make the test fail
+        let traces_sreach:FxHashMap<_,_> = traces_sreach.into_iter().filter(|(k,v)| *v > 0 ).collect();
+
+        println!("traces: {traces:?}");
+        println!("traces: {traces_sreach:?}");
+
+        assert_eq!(traces_sreach, traces);        
+    }
+
+    #[test]
+    fn traces_test_Yeast () {
+        let mut graph = EditGraph::from_gzipped("networks/Yeast.txt.gz").expect("File not found.");
+        graph.remove_loops();
+
+        let degen = DegenGraph::from_graph(&graph);
+        let sreach_oracle = SReachTraceOracle::for_graph(&degen);
+
+        let mut target = vec![379, 772, 1248, 1614, 1635];
+        target.sort_by_key(|x| degen.index_of(x));
+
+        let traces = helper_compute_traces(&target, &degen);
+        let traces_sreach = sreach_oracle.compute_traces(&target, &degen);
+        // println!("traces: {traces:?}");
+        // println!("traces: {traces_sreach:?}");
+
+        for (X, &count) in traces.iter() {
+            assert!(traces_sreach.contains_key(X));
+            assert!(X.is_sorted_by_key(|x| degen.index_of(x)));
+            let sreach_count = traces_sreach[X];
+            if count != sreach_count {
+                println!("Mismatch {X:?} {sreach_count} (should be {count})")
+            }
+            // assert_eq!(count, traces_sreach[X]);
+        }
+
+        assert_eq!(traces_sreach, traces);        
+    }
+
+    fn helper_compute_traces(target:&Vec<Vertex>, degen: &DegenGraph) -> FxHashMap<Vec<Vertex>, usize> {
+        let mut traces:FxHashMap<Vec<Vertex>, usize> = FxHashMap::default();
+        let target:VertexSet = target.iter().cloned().collect();
+        for x in degen.vertices() {
+            if target.contains(x) {
+                continue
+            }
+            let N:VertexSet = degen.neighbours(x).cloned().collect();
+            let mut S:Vec<Vertex> = N.intersection(&target).cloned().collect();
+            S.sort_by_key(|x| degen.index_of(x));
+            *traces.entry(S).or_default() += 1;
+        }
+        traces
+    }
+}
